9415c62e099640450b6a3a06a8d1a1999a0b8e48
Giorgio <gf@Giorgios-MacBook-Air.local>
2026-04-26 10:00:19 +0200

    add guess-indent and indent-blankline plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,20 +30,20 @@ vim.o.undofile = true
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+	vim.o.clipboard = 'unnamedplus'
 end)
 
 -- Copy to clipboard shortcuts
 vim.keymap.set('n', '<leader>cp', function()
-  local path = vim.fn.expand('%:p')
-  vim.fn.setreg('+', path)
-  vim.notify('Copied: ' .. path)
+	local path = vim.fn.expand('%:p')
+	vim.fn.setreg('+', path)
+	vim.notify('Copied: ' .. path)
 end, { desc = 'Copy absolute path' })
 
 vim.keymap.set('n', '<leader>cr', function()
-  local path = vim.fn.expand('%')
-  vim.fn.setreg('+', path)
-  vim.notify('Copied: ' .. path)
+	local path = vim.fn.expand('%')
+	vim.fn.setreg('+', path)
+	vim.notify('Copied: ' .. path)
 end, { desc = 'Copy relative path' })
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
@@ -60,10 +60,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Vim diagnostics
 vim.diagnostic.config({
-  severity_sort = true,          -- show most severe error first
-  update_in_insert = false,      -- don't update while typing
-  float = { source = 'if_many' }, -- nicer look for floats and show source if multiple sources (ex. ruff and ty)
-  jump = { float = true },       -- automatically open the diagnostic float if you jump with [d ]d
+	severity_sort = true,    -- show most severe error first
+	update_in_insert = false, -- don't update while typing
+	float = { source = 'if_many' }, -- nicer look for floats and show source if multiple sources (ex. ruff and ty)
+	jump = { float = true }, -- automatically open the diagnostic float if you jump with [d ]d
 })
 
 -- Show diagnostics
@@ -77,47 +77,49 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- Highlight yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+	group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+	callback = function() vim.highlight.on_yank() end,
 })
 
 -- Plugins
 -- Pack guide: https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack#update
 vim.pack.add({
-  'https://github.com/ibhagwan/fzf-lua',
-  'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/karb94/neoscroll.nvim',
-  'https://github.com/mfussenegger/nvim-dap',
-  'https://github.com/stevearc/oil.nvim',
-  'https://github.com/kdheepak/lazygit.nvim',
-  'https://github.com/esmuellert/codediff.nvim',
-  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-  'https://github.com/kepano/flexoki-neovim',
-  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }, -- pinning so rust binary dependency automatically downloads
+	'https://github.com/ibhagwan/fzf-lua',
+	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/karb94/neoscroll.nvim',
+	'https://github.com/mfussenegger/nvim-dap',
+	'https://github.com/stevearc/oil.nvim',
+	'https://github.com/kdheepak/lazygit.nvim',
+	'https://github.com/esmuellert/codediff.nvim',
+	'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+	'https://github.com/kepano/flexoki-neovim',
+	'https://github.com/NMAC427/guess-indent.nvim',
+	'https://github.com/lukas-reineke/indent-blankline.nvim',
+	{ src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }, -- pinning so rust binary dependency automatically downloads
 })
 
 
 -- FzfLua Setup
 local fzf = require('fzf-lua')
 fzf.setup({
-  keymap = {
-    builtin = {
-      ["<C-d>"] = 'preview-page-down', -- Better scrolling within the displays
-      ["<C-u>"] = 'preview-page-up',
-    },
-  },
-  winopts = {
-    height  = 0.95, -- window height
-    width   = 0.90, -- window width
-    preview = {
-      layout   = 'vertical',
-      vertical = "down:30%",
-    }
-  },
-  files = {
-    formatter = 'path.filename_first',
-  },
+	keymap = {
+		builtin = {
+			["<C-d>"] = 'preview-page-down', -- Better scrolling within the displays
+			["<C-u>"] = 'preview-page-up',
+		},
+	},
+	winopts = {
+		height  = 0.95, -- window height
+		width   = 0.90, -- window width
+		preview = {
+			layout   = 'vertical',
+			vertical = "down:30%",
+		}
+	},
+	files = {
+		formatter = 'path.filename_first',
+	},
 })
 
 vim.keymap.set('n', '<leader><leader>', '<cmd>FzfLua files<cr>', { desc = 'Find files' })
@@ -133,84 +135,84 @@ vim.keymap.set('n', 'gra', fzf.lsp_code_actions, { desc = 'Code actions' })
 -- Treesitter
 vim.cmd('syntax off') -- Make it obvious if treesitter is missing
 vim.api.nvim_create_autocmd('FileType', {
-  callback = function() pcall(vim.treesitter.start) end,
+	callback = function() pcall(vim.treesitter.start) end,
 })
 
 
 -- LSP
 vim.lsp.enable({
-  'ty',                  -- also $ uv tool install ty@latest
-  'ruff',                -- also $ uv tool install ruff@latest
-  'lua_ls'               -- also $ brew install lua-language-server
+	'ty',            -- also $ uv tool install ty@latest
+	'ruff',          -- also $ uv tool install ruff@latest
+	'lua_ls'         -- also $ brew install lua-language-server
 })
 vim.o.signcolumn = 'yes' -- make lsp warnings not widen the gutter
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 -- Auto-format ("lint") on save (adapted from neovim docs :help auto-format)
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
-  callback = function(ev)
-    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-    if not client:supports_method('textDocument/willSaveWaitUntil')
-        and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp.fmt', { clear = false }),
-        buffer = ev.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
-  end,
+	group = vim.api.nvim_create_augroup('my.lsp', { clear = true }),
+	callback = function(ev)
+		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+		if not client:supports_method('textDocument/willSaveWaitUntil')
+		    and client:supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('my.lsp.fmt', { clear = false }),
+				buffer = ev.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+				end,
+			})
+		end
+	end,
 })
 
 
 -- Blink.cmp
 require('blink.cmp').setup({
-  keymap = {
-    preset = "enter",
-  }
+	keymap = {
+		preset = "enter",
+	}
 })
 
 
 -- Neoscroll
 require('neoscroll').setup({
-  hide_cursor = false,
-  stop_eof = true,
-  easing = 'quadratic',
-  duration_multiplier = 0.30,
+	hide_cursor = false,
+	stop_eof = true,
+	easing = 'quadratic',
+	duration_multiplier = 0.30,
 })
 
 
 -- Dap (debugging)
 local dap = require('dap')
 dap.adapters.debugpy = function(cb, config) -- also $ uv tool install debugpy@latest
-  if config.request == 'attach' then
-    cb({
-      type = 'server',
-      port = config.connect.port,
-      host = config.connect.host or '127.0.0.1',
-    })
-  else
-    cb({
-      type = 'executable',
-      command = 'debugpy-adapter',
-    })
-  end
+	if config.request == 'attach' then
+		cb({
+			type = 'server',
+			port = config.connect.port,
+			host = config.connect.host or '127.0.0.1',
+		})
+	else
+		cb({
+			type = 'executable',
+			command = 'debugpy-adapter',
+		})
+	end
 end
 dap.configurations.python = { -- https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
-  {
-    type = 'debugpy',
-    request = 'launch',
-    name = 'Launch file',
-    program = '${file}',
-    python = function()
-      local root = vim.fs.root(0, '.venv')
-      return { root and root .. '/.venv/bin/python' or 'python3' }
-    end,
-    cwd = function()
-      return vim.fs.root(0, '.venv') or vim.fn.getcwd()
-    end,
-  },
+	{
+		type = 'debugpy',
+		request = 'launch',
+		name = 'Launch file',
+		program = '${file}',
+		python = function()
+			local root = vim.fs.root(0, '.venv')
+			return { root and root .. '/.venv/bin/python' or 'python3' }
+		end,
+		cwd = function()
+			return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+		end,
+	},
 }
 -- vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug toggle breakpoint' })
 -- vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug continue' })
@@ -226,9 +228,9 @@ dap.configurations.python = { -- https://github.com/microsoft/debugpy/wiki/Debug
 
 -- Oil.nvim
 require("oil").setup({
-  view_options = {
-    show_hidden = true,
-  },
+	view_options = {
+		show_hidden = true,
+	},
 })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
@@ -252,3 +254,11 @@ require('render-markdown').setup({})
 require('flexoki').setup({})
 vim.cmd('colorscheme flexoki-dark') -- need to call after setup
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+
+
+-- guess-indent
+require('guess-indent').setup({})
+
+
+-- indent-blankline
+require('ibl').setup({})
